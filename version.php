9004947d63097373c84c61cdbdbd33d5aa2d71a2
Matthew Miller <matthewmiller210@gmail.com>
2015-03-20 10:13:14 -0600

    Added Index
    Updated the index page to actual index for the course

--- version.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$plugin->version   = 2015030200; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014051200; // Requires this Moodle version
-$plugin->cron      = 0; // Period for cron to check this module (secs)
-$plugin->component = 'mod_certificate';
-
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "Stable"; // User-friendly version number
