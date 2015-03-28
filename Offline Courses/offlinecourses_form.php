2134d200fee098d7179a823c778320b46c24b67b
Kellen Lask <kellenlask@gmail.com>
2015-03-27 19:13:09 -0600

    Offline Courses
    Added the skeleton to create a form plugin to add Offline Courses

---
+++ Offline Courses/offlinecourses_form.php
@@ -0,0 +1,18 @@
+<?php
+
+class offlinecourses_form extends moodleform {
+
+    public function definition() {
+        
+    }
+
+//End public function definition() 
+
+    public function validation($data, $files) {
+        
+    }
+
+//End public function validation($data, $files)
+}
+
+?>
