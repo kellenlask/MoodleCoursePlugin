439eece7fffc886df9bc1931c633df861c710cca
Kellen Lask <kellenlask@gmail.com>
2015-03-29 18:37:22 -0600

    Offline Courses
    Setup offline courses form.

---
+++ Offline Courses/index.php
@@ -0,0 +1,48 @@
+<?php 
+require_once('../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+
+$courseid = required_param('course', PARAM_INT); //The course ID
+$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST); //Grab the course from the database using the course ID
+
+//Require Login and admin 
+require_login();
+isadmin();
+
+$PAGE->set_context(context_system::instance());
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title('Set Course Price');
+$PAGE->set_heading('Set Course Price');
+$PAGE->set_url($CFG->wwwroot.'/course/prices/index.php');
+echo $OUTPUT->header();
+?>
+
+<form method="post" action="post.php">
+        <h2>Set Course Discount</h2><br>
+        <label for="currentDiscount">Current Discount: </label>
+		<input name="currentDiscount" type="number" readonly />
+		
+		<br>
+		<label for="Discount">Cost: $</label> 
+        <input name="price" type="number" min="0" max="1000" />
+    
+		
+	
+	
+        <br>
+        <input type="submit" value="Download Records">
+</form>
+
+
+<?php
+
+// Trigger a content view event.
+$event = \report_stats\event\report_viewed::create(array('context' => $context, 'relateduserid' => $userid,
+        'other' => array('report' => $report, 'time' => $time, 'mode' => $mode)));
+$event->trigger();
+
+
+
+echo $OUTPUT->footer();
+
+?>
